60a4557ddbdf7b9c1debc3c481a42ebe83399a8c
Arnest Allka <arisallkas@gmail.com>
2024-08-13 15:28:01 +0300

    Mongo setup

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from website import create_app
+from pymongo import MongoClient
 
 app = create_app()
+client = MongoClient('localhost', 27017)
+db = client['database']
+users_collection = db['users']
+
 
 if __name__ == '__main__':
     app.run(debug=True)
