96f62a1b14d0338e2f5e3997f433c254f432f932
Arnest Allka <arisallkas@gmail.com>
2024-09-23 19:12:08 +0300

    Containerization of the app with docker compose up done

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or "a_very_secret_key"
-    MONGO_URI = os.environ.get('MONGO_URI') or "mongodb://localhost:27017/flask_mongo"
+    SECRET_KEY = os.environ.get('SECRET_KEY', "a_very_secret_key")
+    SERVER_HOST = os.environ.get('SERVER_HOST', 'localhost')
+    SERVER_PORT = int(os.environ.get('SERVER_PORT', 5000))
+    MONGO_URI = os.environ.get('MONGO_URI', "mongodb://localhost:27017/DigiMeet")
